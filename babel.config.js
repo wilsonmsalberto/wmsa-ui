83d96fa563b4eae93c6941eb2e34f6849f379fcc
Wilson Alberto <wilsonalberto@gmail.com>
2019-08-31 16:55:44 +0100

    Initial babel setup

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  env: {
+    'build': {
+      ignore: [
+        "./**/*.story.{js,jsx}",
+        "./**/*.spec.{js,jsx}",
+        "./**/*.snap",
+      ]
+    }
+  },
+  ignore: ['node_modules'],
+};
